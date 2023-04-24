3b6b85b333110456b7e79f92cea4a6ad04eacfb8
Krxnky <rylanelarson@gmail.com>
2023-04-24 12:33:40 -0500

    create basic ui elements on main game

---
+++ DBBLClient/src/actors/HealthBar.java
@@ -0,0 +1,33 @@
+package actors;
+
+import greenfoot.Actor;
+import greenfoot.Color;
+import greenfoot.GreenfootImage;
+
+public class HealthBar extends Actor {
+    private int health = 0;
+    private int healthBarWidth = 300;
+    private int healthBarHeight = 50;
+
+    public HealthBar(int health)
+    {
+        this.health = health;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void act()
+    {
+        setImage(new GreenfootImage(healthBarWidth+2, healthBarHeight+2));
+        getImage().setColor(Color.BLACK);
+        getImage().drawRect(0, 0, healthBarWidth+1, healthBarHeight+1);
+        getImage().setColor(Color.RED);
+        getImage().fillRect(1, 1, health*3, healthBarHeight);
+    }
+}
--- DBBLClient/src/worlds/MainGame.java
+++ DBBLClient/src/worlds/MainGame.java
@@ -1,10 +1,33 @@
 package worlds;
 
+import actors.HealthBar;
+import greenfoot.GreenfootImage;
 import greenfoot.World;
 
 public class MainGame extends World  {
+    HealthBar playerHealthBar;
+    HealthBar enemyHealthBar;
+
     public MainGame()
     {
         super(1000, 600, 1, false);
+
+        GreenfootImage bg = new GreenfootImage("MV5BMWM3MmY2YWEtNjVmZi00NmZlLTliN2EtYTc0YzhhOTQ3MzE2XkEyXkFqcGdeQXVyNzgxMzc3OTc@._V1_.png");
+        bg.scale(1000, 600);
+        setBackground(bg);
+
+        buildUI();
+    }
+
+    public void buildUI()
+    {
+        // Create Healthbar UI actors
+        playerHealthBar = new HealthBar(100);
+        enemyHealthBar = new HealthBar(100);
+
+        addObject(playerHealthBar, playerHealthBar.getImage().getWidth() + 150, 30);
+        addObject(enemyHealthBar, getWidth() - enemyHealthBar.getImage().getWidth() - 150, 30);
+
+
     }
 }
