8564c40cc371d29836d3247bb0680a0a27dbca78
111356 <cjmcclasky@gmail.com>
2023-04-25 12:20:09 -0500

    serv code //commented

--- DBBLServer/src/Server.java
+++ DBBLServer/src/Server.java
@@ -1,2 +1,13 @@
 public class Server {
+    public Server(){
+
+    }
+
+//    public static String attackCompare(Player.ATTACK_TYPE attackType){
+//
+//        return "";
+//    }
+//    public static String clashHandler (int a, int b){
+//        return Math.max(a,b) == a ? "GokuHit" : "VegetaHit";
+//    }
 }
