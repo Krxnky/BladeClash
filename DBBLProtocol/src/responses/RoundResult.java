5b69e9a567be306e4e7356fe6a43a85f4b9b16cb
Krxnky <rylanelarson@gmail.com>
2023-05-01 12:17:46 -0500

    game client code

---
+++ DBBLProtocol/src/responses/RoundResult.java
@@ -0,0 +1,24 @@
+package responses;
+
+import requests.PlayerInfo;
+
+import java.io.Serializable;
+
+public class RoundResult implements Serializable {
+    private final GameInfo UpdatedGameInfo;
+    private final PlayerInfo UpdatedLocalPlayerInfo;
+
+    public RoundResult(GameInfo updatedGameInfo, PlayerInfo updatedLocalPlayerInfo)
+    {
+        this.UpdatedGameInfo = updatedGameInfo;
+        this.UpdatedLocalPlayerInfo = updatedLocalPlayerInfo;
+    }
+
+    public GameInfo getUpdatedGameInfo() {
+        return UpdatedGameInfo;
+    }
+
+    public PlayerInfo getUpdatedLocalPlayerInfo() {
+        return UpdatedLocalPlayerInfo;
+    }
+}
